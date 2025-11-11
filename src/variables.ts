ecc00ac69add667350cae15b04fa4ed957d30bae
GIT-Marquestra <vabhi.verma2678@gmail.com>
2025-11-11 19:38:17 +0530

    done

--- src/variables.ts
+++ src/variables.ts
@@ -1,4 +1,4 @@
 export const cloudinaryString="cloudinary://<your_api_key>:<your_api_secret>@dutrfbtao"
-export const backendString="https://backend-chat-app-1-6hh7.onrender.com"
+export const backendString="https://backend-chat-app-53zg.onrender.com"
 export const wsString="https://chatappwebsocket-8.onrender.com"
 // nice
